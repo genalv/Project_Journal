caeef3305e596c7607f6522e90b70d1d25dca062
genalv <genepatrickalvarez@gmail.com>
2022-11-03 18:46:37 +0800

    test deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,5 +120,5 @@ Rails.application.configure do
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.hosts << projectjournal-production.up.railway.app
+  config.hosts << "projectjournal-production.up.railway.app"
 end
